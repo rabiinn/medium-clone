8378239bab5627fb50cd98028db943a69912749c
rabiinn <rabin.adhikari@mni.thm.de>
2025-07-30 00:48:44 +0200

    Initialize the server

---
+++ server/app.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import { Router } from 'express'
+const app = express()
+
+const router = Router();
+
+router.get('/hello', (req, res) => {
+    res.status(200).json({message: "Hello World"});
+})
+
+app.use(router);
+
+
+export default app;
+
+
+
